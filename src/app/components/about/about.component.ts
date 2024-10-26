021ec2d388b50e0350d8df06de8285868f9b353c
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-10-27 09:15:56 +1300

    complete about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowTrendUp, faBookOpenReader, faHardHat, faPalette, faTreeCity, faWifiStrong } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  public upTrend = faArrowTrendUp;
+  public bookReader = faBookOpenReader;
+  public trees = faTreeCity;
+  public engineer = faHardHat;
+  public wifi = faWifiStrong;
+  public palette = faPalette;
 }
