84cb7039ef8e0c4299b777c56e96ac36618ae188
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-10-28 11:25:01 +1300

    complete navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
@@ -19,13 +19,52 @@ import { trigger, style, transition, animate } from '@angular/animations';
       ]),
       transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
     ]),
+    trigger('navbarInOut', [
+      transition(':enter', [
+        style({ top: '-100px' }),
+        animate('500ms ease-out', style({ top: 0 })),
+      ]),
+      transition(':leave', [
+        animate('500ms ease-out', style({ top: '-100px' })),
+      ]),
+    ]),
   ],
 })
 export class NavbarComponent {
   public menu = faBars;
   public close = faClose;
   public showDrawer = false;
+  public isNavbarVisible = true;
+
+  private isScrolling = false;
+  private lastScrollTop = 0;
+
   public toggleDrawer() {
     this.showDrawer = !this.showDrawer;
+  }
+  public navigateToSection(sectionId: string) {
+    if (sectionId !== 'landing') {
+      this.isScrolling = true;
+      this.isNavbarVisible = false;
+    }
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+    this.showDrawer = false;
+    setTimeout(() => {
+      this.isScrolling = false;
+    }, 1000);
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > this.lastScrollTop) {
+      // Scrolling down
+      this.isNavbarVisible = false;
+    } else {
+      // Scrolling up
+      if (!this.isScrolling) this.isNavbarVisible = true;
+    }
+
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
   }
 }
