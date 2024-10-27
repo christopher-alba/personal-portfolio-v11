f61262c5f879591f9b628972294d648bc2c2f311
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-10-28 09:06:02 +1300

    complete skills section

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 import { TabsComponent } from '../tabs/tabs.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
+import { TechnicalComponent } from './technical/technical.component';
+import { SoftComponent } from './soft/soft.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [TabComponent, TabsComponent, TechnologiesComponent],
+  imports: [
+    TabComponent,
+    TabsComponent,
+    TechnologiesComponent,
+    TechnicalComponent,
+    SoftComponent,
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
---
+++ src/app/components/skills/soft/soft.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soft',
+  standalone: true,
+  imports: [],
+  templateUrl: './soft.component.html',
+  styleUrl: './soft.component.scss'
+})
+export class SoftComponent {
+
+}
